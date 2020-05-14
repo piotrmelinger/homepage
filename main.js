7d50b62eeabfcfff78dd7d1a34737458d148355c
Piotr Melinger <piotr.melinger@gmail.com>
2020-05-14 14:29:26 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const firstName = 'Piotr';
+const age = 32;
+const city = 'Poznań';
+const JS = 'JavaScript';
+const HTML = 'HTML';
+const CSS = 'CSS';
+console.log('heloł ewryłan');
+console.log(`Cześć, mam na imię ${firstName}, urodziłem się w ${city} i mam ${age} lata`);
